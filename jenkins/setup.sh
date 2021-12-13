face6b437584e2730cc05e2627d4845968c4cb51
Ubuntu <mutlu@mutlu-jenkins.d5cfe1on2sve3o21wdclcojfre.bx.internal.cloudapp.net>
2021-12-13 16:22:32 +0000

    updating jenkinsfile

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-mkdir newdir
-cd newdir
-touch newfile
+sudo apt update
+sudo apt install python3 python3-venv python3-pip -y
+
+# create and activate venv
+python3 -m venv venv
+source venv/bin/activate
+
+# install pip requirements
+pip3 install -r requirements.txt
